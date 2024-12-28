0f47d3a17dd26b1d4ad6efcbf0bda14ac6c3a4e3
Muatasim Qazi <muatasimqazi@gmail.com>
2024-12-27 18:25:44 -0800

    add basic details of website

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
 export default function Home() {
   return (
-    <div className="container">
+    <div className="container w-[500px] mx-auto">
       <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-xl">Al-Watan Halal Meats & Market - Kirkland, WA</h1>
-        <p className="text-md">Website coming soon...</p>
+        <h1 className="text-xl font-extrabold mb-4">
+          Al-Watan Halal Meats & Market - Kirkland, WA
+        </h1>
+        <p className="text-md">
+          Discover a wide selection of authentic Indian, Pakistani, Middle
+          Eastern, and African goods and groceries. Shop fresh produce, spices,
+          halal products, and specialty items that bring the flavors of home to
+          your kitchen.
+        </p>
+        <p className="text-m font-bold my-4">Website coming soon...</p>
+        <p className="text-md my-2">1421 Market St, Kirkland WA 98033</p>
+        <p className="text-md">
+          <a href="tel:v">Call us at: 425-822-9772</a>
+        </p>
+        <p className="text-md">
+          Mon-Sat: 9:30 am to 9:00 pm
+          <br />
+          Sun: 11:00 am to 7:00 pm
+        </p>
       </div>
     </div>
   );
